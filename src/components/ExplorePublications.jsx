0445d55744b980d1a4a9d062b71f2578cf9068c3
REALcuba <angelskcuba@gmail.com>
2023-01-22 14:55:00 +0100

    card render name

--- src/components/ExplorePublications.jsx
+++ src/components/ExplorePublications.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { explorePublications } from '../lensQueries/explorePublications'
+import Card from './Card/Card'
 // import Card from './Card/Card'
 import './style.css'
 export default function ExplorePublications (props) {
   const [cards, setCards] = useState([])
+  const [img, setImg] = useState({})
 
   const init = async () => {
     try {
@@ -18,8 +20,9 @@ export default function ExplorePublications (props) {
       const response = await explorePublications(request) // To get next result replace the cursor with the value of response.pageInfo.next
       // console.log(response)
       const responseArr = response.data.explorePublications.items
-      // console.log(responseArr)
+      console.log(responseArr)
       setCards(responseArr)
+      // setImg(responseArr.profile.picture)
     } catch (err) {
       console.log(err)
     }
@@ -31,11 +34,24 @@ export default function ExplorePublications (props) {
 
   return (
 
-    <div>
+    <div className='container row justify-content-center'>
 
       {cards.map((card, index) => {
-        console.log(index, card.id, card.profile.name)
-        return <li key={card.id}>{card.id}</li>
+        // console.log(index, card.id, card.profile.name)
+        // console.log(img.original.url)
+        return (
+
+          <div className='card rounded justify-content-center' style={{ width: 250, height: 250 }} key={card.id}>
+            <img
+              className='card-img' src={card.ownedBy} alt=''
+            />
+            <div className='card-body ' key={card.id}>
+              <h3 className='card-text'>{card.profile.name}</h3>
+            </div>
+            {/* <Card key={card.id} /> */}
+          </div>
+
+        )
       })}
     </div>
   )
