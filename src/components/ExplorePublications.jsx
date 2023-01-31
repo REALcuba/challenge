26e47cbc13cd8fdeacb59b97d254702d9aa08a9e
REALcuba <angelskcuba@gmail.com>
2023-01-31 21:45:28 +0100

    add event listener, but not loading the right card

--- src/components/ExplorePublications.jsx
+++ src/components/ExplorePublications.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
+
 import { explorePublications } from '../lensQueries/explorePublications'
 import PaintCardInfo from '../components/PaintCardInfo/PaintCardInfo'
 import './style.css'
-// import inbox from '../assets/svg/inbox.svg'
+
 export default function ExplorePublications (props) {
   const [cards, setCards] = useState([])
-  // const [loading, setLoading] = useState(false)
-  // const [hoverImg, setHoverImg] = useState(false)
-  // const [img, setImg] = useState()
-  // const [className, setClassName] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [cursor, setCursor] = useState('{"timestamp":1,"offset":0}')
+  // const [response, setResponse] = useState([])
+  // const [page, setPage] = useState(1)
 
   const init = async () => {
     try {
@@ -17,17 +18,16 @@ export default function ExplorePublications (props) {
         noRandomize: true,
         sources: ['5bba5781-78b5-4927-8d2f-122742817583'],
         publicationTypes: ['POST'],
-        cursor: '{"timestamp":1,"offset":0}',
-        limit: 24
+        cursor: { cursor },
+        limit: 6
       }
       const response = await explorePublications(request) // To get next result replace the cursor with the value of response.pageInfo.next
-      const responseArr = response.data.explorePublications.items
       console.log(response)
-      // console.log(responseArr)
+      const responseArr = response.data.explorePublications.items
+      console.log(cursor)
+      setCursor(response.data.explorePublications.pageInfo.next)
       setCards(responseArr)
-
-      // setLoading(true)
-      // setImg(responseArr.profile.picture.original.url)
+      return response
     } catch (err) {
       console.log(err)
     }
@@ -37,20 +37,37 @@ export default function ExplorePublications (props) {
     init()
   }, [])
 
+  const handleScroll = async () => {
+    setLoading(true)
+    const getNewCards = await init().then(response => {
+      const newCards = response.data.explorePublications.items
+      console.log('newCards:', response.data.explorePublications.items)
+      setCards([...cards, ...newCards])
+    })
+    if (
+      window.innerHeight + document.documentElement.scrollTop <=
+      document.documentElement.offsetHeight
+    ) {
+      return
+    }
+    // init()
+    getNewCards()
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [cards, loading])
+
   return (
-
     <div className='container-fluid row bg-black m-0 justify-content-center'>
-
       {cards.map((card) => {
-        // const URL = card.metadata.media[0].original.url
-        // const imgUrl = URL => URL.replace('ipfs:', 'https://ipfs.io/ipfs/')
         return (
           <div className='card p-0 m-1' key={card.id}>
-            <PaintCardInfo key={card.id}>{card}</PaintCardInfo>
+            <PaintCardInfo onClick={handleScroll}>{card}</PaintCardInfo>
+            {loading && <div>loading...</div>}
           </div>
         )
       })}
-
     </div>
   )
 }
